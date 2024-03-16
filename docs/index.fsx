945564d9af36f5d27223426008bb65a79897d7b7
Fxplorer <154011238+Fxplorer@users.noreply.github.com>
2024-03-15 21:09:57 -0400

    Initial commit

---
+++ docs/index.fsx
@@ -0,0 +1,24 @@
+(**
+What is FsUnit?
+===============
+
+**FsUnit** is a set of libraries that makes unit-testing with F# more enjoyable. It adds a special syntax to your favorite .NET testing framework.
+FsUnit currently supports NUnit, xUnit, and MsTest.
+
+The goals of FsUnit are:
+
+* to make unit-testing feel more at home in F# , i.e., more functional.
+* to leverage existing test frameworks while at the same time adapting them to the F# language in new ways.
+
+NuGet packages are available for each of the supported testing frameworks:
+
+* [![NuGet Status](https://buildstats.info/nuget/FsUnit) - NUnit](https://www.nuget.org/packages/FsUnit/)
+* [![NuGet Status](https://buildstats.info/nuget/FsUnit.Xunit) - xUnit](https://www.nuget.org/packages/FsUnit.Xunit/)
+* [![NuGet Status](https://buildstats.info/nuget/Fs30Unit.MsTest) - MsTest](https://www.nuget.org/packages/Fs30Unit.MsTest/)
+
+
+Syntax
+-------
+
+With FsUnit, you can write unit tests like this:
+*)
